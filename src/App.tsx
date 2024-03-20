6464fe3b3dfc75115b89cee90511a4a1245f31ed
ricky <richardwoesiii@gmail.com>
2024-03-20 12:29:10 -0400

    Fix import path and add SignUp component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Test from './features/test/Test';
 import MyWorkout from './features/myworkouts/MyWorkout';
 import Workoutlogs from './features/workoutlogs/WorkoutLogs';
 import Auth from './features/auth/Auth';
-import SignUp from './features/auth/signUp.tsx/SignUp';
+import SignUp from './features/auth/signUp/SignUp';
 import Messages from './features/messages/Messages';
 
 import '@fontsource/roboto/300.css';
